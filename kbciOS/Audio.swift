17ee2c9c91ea7647e3f3022b9d0ec381aef8c980
Chris Magesty <cmagesty@gmail.com>
2017-08-05 19:22:45 -0400

    Created an Audio Class
    -The audio class will contain the preacher and audio objects

---
+++ kbciOS/Audio.swift
@@ -0,0 +1,21 @@
+//
+//  Audio.swift
+//  kbciOS
+//
+//  Created by Christopher Magesty on 8/5/17.
+//  Copyright © 2017 Christopher Magesty. All rights reserved.
+//
+
+import UIKit
+
+class Audio: NSObject {
+    var sermonDate : String?
+    
+}
+
+class Preacher: NSObject {
+    var preacherThumbnailImage : String?
+    var preacherName : String?
+    var sermonLanguage: String?
+    
+}
